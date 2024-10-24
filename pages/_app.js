6dc814041ce31e05927e3ad55b1bf725267d0d18
trent <trntannan1@gmail.com>
2024-10-24 15:35:53 +1300

    small

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-
-import React from 'react';
-import '../styles/globals.css';
-import { useState } from 'react';
+import React from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
