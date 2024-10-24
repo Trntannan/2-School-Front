5f0332e5b9a4130d87d65f1b9c9c744583bbfab3
trent <trntannan1@gmail.com>
2024-10-24 17:55:40 +1300

    sil

--- components/Signup.jsx
+++ components/Signup.jsx
@@ -29,7 +29,7 @@ const Signup = () => {
 
     try {
       const response = await axios.post(
-        `${backendUrl}/api/user/signup`,
+        `${backendUrl}/api/user/register`,
         {
           username,
           email,
