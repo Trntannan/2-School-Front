da3028ad94a65a0bf51837c7bf42d31dd40ce4fe
trent <trntannan1@gmail.com>
2024-10-24 17:50:56 +1300

    api route change

--- components/Signup.jsx
+++ components/Signup.jsx
@@ -29,7 +29,7 @@ const Signup = () => {
 
     try {
       const response = await axios.post(
-        `${backendUrl}/api/users/signup`,
+        `${backendUrl}/api/user/signup`,
         {
           username,
           email,
