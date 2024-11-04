10d38a0022832eddc68e85b8a6c3567d8f78b183
trent <trntannan1@gmail.com>
2024-11-04 22:56:53 +1300

    boo

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-  env: {
-    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-    BACKEND_URL: process.env.BACKEND_URL,
-  },
-};
-
 export default nextConfig;
