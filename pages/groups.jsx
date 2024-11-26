f023b2b1e09c1d9243e4f9745cacd601c6722e9a
trent <trntannan1@gmail.com>
2024-11-26 17:25:51 +1300

    'Processing...' while awaiting response from client

--- pages/groups.jsx
+++ pages/groups.jsx
@@ -13,6 +13,7 @@ const Groups = () => {
   const [showNewGroupForm, setShowNewGroupForm] = useState(false);
   const [map, setMap] = useState(null);
   const [mapsApi, setMapsApi] = useState(null);
+  const [isClient, setIsClient] = useState(false);
 
   const fetchGroups = async () => {
     const token = localStorage.getItem("token");
@@ -31,6 +32,7 @@ const Groups = () => {
 
   useEffect(() => {
     fetchGroups();
+    setIsClient(true);
   }, []);
 
   const handleGroupClick = (group) => {
@@ -55,11 +57,12 @@ const Groups = () => {
   const handleDeleteGroup = async (groupId) => {
     try {
       const token = localStorage.getItem("token");
-      await axios.delete(`${backendUrl}/api/user/delete-group/${groupId}`, {
+      await axios.delete(`${backendUrl}/api/user/delete-group`, {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
+        data: { groupId },
       });
       fetchGroups();
     } catch (error) {
@@ -67,10 +70,41 @@ const Groups = () => {
     }
   };
 
-  // const getIndicatorStyle = (isActive) =>
-  //   `${
-  //     isActive ? "bg-green-500" : "bg-white"
-  //   } h-4 w-4 rounded-full border border-gray-300 inline-block`;
+  if (!isClient || groups.length === 0) {
+    return (
+      <div className="page-container">
+        <main className="main-content flex items-center justify-center">
+          <button
+            type="button"
+            className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed"
+            disabled
+          >
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+              ></path>
+            </svg>
+            Processing...
+          </button>
+        </main>
+      </div>
+    );
+  }
 
   return (
     <div className="page-container">
