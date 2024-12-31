e18ad13906d8b08a19e0afb9c5852c2184728586
trent <trntannan1@gmail.com>
2025-01-01 08:04:33 +1300

    added badges

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/globals.css";
 import { useRouter } from "next/router";
 import PageHeader from "../components/pageHeader";
 import BottomNavBar from "../components/BottomNavBar";
+import Image from "next/image";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const isIndexPage = router.pathname === "/";
+  const [userTier, setUserTier] = useState(null);
+
+  useEffect(() => {
+    if (!isIndexPage) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const decodedToken = JSON.parse(atob(token.split(".")[1]));
+        setUserTier(decodedToken.tier);
+      }
+    }
+  }, [isIndexPage]);
+
+  const getTierImage = (tier) => {
+    const tierImages = {
+      BRONZE: "/media/Tiers/bronze.jpeg",
+      SILVER: "/media/Tiers/silver.jpeg",
+      GOLD: "/media/Tiers/gold.jpeg",
+      DIAMOND: "/media/Tiers/diamond.jpeg",
+    };
+    return tierImages[tier];
+  };
 
   const pageTitles = {
     "/profile": "Profile",
@@ -19,7 +41,21 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      {!isIndexPage && <PageHeader title={pageTitles[router.pathname]} />}
+      {!isIndexPage && (
+        <div>
+          <PageHeader title={pageTitles[router.pathname]} />
+          {userTier && (
+            <div className="fixed right-4 top-4 z-50">
+              <Image
+                src={getTierImage(userTier)}
+                alt={`${userTier}`}
+                width={32}
+                height={32}
+              />
+            </div>
+          )}
+        </div>
+      )}
 
       <main
         className={`pt-${!isIndexPage ? 16 : 0} mb-${!isIndexPage ? 12 : 0}`}
