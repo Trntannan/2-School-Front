f400abbda6b3af08d8a4b1bbb5ecea1bf48383e4
trent <trntannan1@gmail.com>
2025-01-01 18:04:59 +1300

    store tier badges locally

--- pages/_app.js
+++ pages/_app.js
@@ -2,54 +2,32 @@ import React, { useState, useEffect } from "react";
 import "../styles/globals.css";
 import { useRouter } from "next/router";
 import PageHeader from "../components/pageHeader";
-import axios from "axios";
-import jwtDecode from "jwt-decode";
 import BottomNavBar from "../components/BottomNavBar";
 import Image from "next/image";
-
-const backendUrl = "https://two-school-backend.onrender.com" || 5000;
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const isIndexPage = router.pathname === "/";
   const [userTier, setUserTier] = useState(null);
-  const [tierImageURL, setTierImageURL] = useState(null);
 
   useEffect(() => {
     if (!isIndexPage) {
       const token = localStorage.getItem("token");
       if (token) {
-        const decodedToken = jwtDecode(token);
+        const decodedToken = JSON.parse(atob(token.split(".")[1]));
         setUserTier(decodedToken.tier);
       }
     }
   }, [isIndexPage]);
 
-  useEffect(() => {
-    if (userTier) {
-      const loadTierImage = async () => {
-        const imageUrl = await getTierImage(userTier);
-        setTierImage(imageUrl);
-      };
-      loadTierImage();
-    }
-  }, [userTier]);
-
-  const getTierImage = async () => {
-    try {
-      const token = localStorage.getItem("token");
-      const response = await axios.get(`${backendUrl}/api/tierImages/all`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      setTierImageUrl(response.data.tierImages);
-      return response.data.tierImages[userTier];
-    } catch (error) {
-      console.error("Error fetching tier image:", error);
-      return null;
-    }
+  const getTierImage = (tier) => {
+    const tierImages = {
+      BRONZE: "/media/Tiers/bronze.png",
+      SILVER: "/media/Tiers/silver.png",
+      GOLD: "/media/Tiers/gold.png",
+      DIAMOND: "/media/Tiers/diamond.png",
+    };
+    return tierImages[tier];
   };
 
   const pageTitles = {
@@ -66,16 +44,13 @@ function MyApp({ Component, pageProps }) {
       {!isIndexPage && (
         <div>
           <PageHeader title={pageTitles[router.pathname]} />
-          {userTier && tierImageUrl && (
-            <div className="fixed right-3 z-50">
+          {userTier && (
+            <div className="fixed right-3  z-50">
               <Image
-                src={`data:image/png;base64,${
-                  tierImageUrl[userTier.toLowerCase()]
-                }`}
-                alt={`${userTier} Tier`}
+                src={getTierImage(userTier)}
+                alt={`${userTier}`}
                 width={55}
                 height={47}
-                priority
               />
             </div>
           )}
