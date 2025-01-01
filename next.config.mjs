d262f7991c51bdd50a2107af7991213d84547a4f
trent <trntannan1@gmail.com>
2025-01-01 19:33:59 +1300

    more

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "https://two-school-backend.onrender.com",
-        pathname: "/tTiers/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/Tiers/**",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
